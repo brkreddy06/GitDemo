2647d6e1c45235549bcd1b246b54dee9bbbf7906
Ram <brkreddy06@gmail.com>
2020-07-28 14:24:20 +0530

    added new file

---
+++ GitPractice.py
@@ -0,0 +1,19 @@
+str = "RamkumarAcademy.com"
+str1 = "Reddy"
+str2 = "ram"
+print(str[0])  # to print first letter R in the string
+print(str[3:8])  # it prints kumar
+print(str[-1])  # prints last letter m
+
+print(str + str1)  # concatenation
+
+var = str.split(".")  # split is a command to split the string
+print(var)
+print(var[0])
+
+print(str2 in str)  # to check the string2 value is present in string1. Substring check
+
+str3 = " Welcome "
+print(str3.strip())  # it removes the spaces from both start and end
+print(str3.lstrip())  # it removes lest side space
+print(str3.rstrip())  # it removes right side space
